0177e11abb775bc919ab678edcc5e3cd307da64d
Louis Chatriot <louis.chatriot@gmail.com>
2017-12-06 17:52:02 +0100

    Line by line analysis and saving

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cp = require('child_process')
     , async = require('async')
     , Papa = require('papaparse')
     , domainsToTryFile = 'to-try.txt'   // Separated by newlines
-    , resultsFile = "results.csv"
+    , resultsFile = domainsToTryFile.replace(/\..*$/, '') + "-results.csv"
     ;
 
 var _data = fs.readFileSync('data/' + domainsToTryFile, 'utf8');
@@ -33,6 +33,17 @@ data = data.map(d => d.replace(/[üûù]/g, 'u'));
 data = data.map(d => d.replace(/[ç]/g, 'c'));
 
 
+// Data will be appended after every full domain analysis
+// to be able to stop the script whenever we want
+var fields = ['domain', 'available', 'regOrg', 'regPhone', 'regEmail'];
+fs.writeFileSync('data/' + resultsFile, fields.join(',') + eol, 'utf8');
+function saveNewDomain (d) {
+  var csvified = Papa.unparse({ data: [d], fields: fields }, { header: false });
+  fs.appendFileSync('data/' + resultsFile, csvified + eol, 'utf8');
+}
+
+
+
 async.eachOfSeries(data, function (domain, k, cb) {
   process.stdout.write("Analyzing domain " + domain + " ...");
 
@@ -40,31 +51,30 @@ async.eachOfSeries(data, function (domain, k, cb) {
   cp.exec("whois " + domain, {}, function (err, sout, serr) {
     if (sout.match(/No match for/)) {
       console.log(" free");
-      results.push({ domain: domain, available: 'yes' });
+      saveNewDomain({ domain: domain, available: 'yes' });
       return cb();
     } else {
       console.log(" taken");
 
       // Find and query registrar WHOIS server
-      var registrarWHOIS = sout.match(new RegExp("Registrar WHOIS Server: ([^" + eolToString + "]*)"));
+      var registrarWHOIS = sout.match(new RegExp("Registrar WHOIS Server: ?([^" + eolToString + "]*)"));
       registrarWHOIS = registrarWHOIS ? (registrarWHOIS[1] ? (registrarWHOIS[1].length > 0 ? registrarWHOIS[1] : '') : '') : '';
 
       if (registrarWHOIS === '') {
-        results.push({ domain: domain, available: 'no' });
+        saveNewDomain({ domain: domain, available: 'no' });
         return cb();
       }
 
       cp.exec("whois " + domain + " -h " + registrarWHOIS , {}, function (err, sout, serr) {
-        var org = sout.match(new RegExp("Registrant Organization: ([^" + eolToString + "]*)"))
-          , phone = sout.match(new RegExp("Registrant Phone: ([^" + eolToString + "]*)"))
-          , email = sout.match(new RegExp("Registrant Email: ([^" + eolToString + "]*)"))
+        var org = sout.match(new RegExp("Registrant Organization: ?([^" + eolToString + "]*)"))
+          , phone = sout.match(new RegExp("Registrant Phone: ?([^" + eolToString + "]*)"))
+          , email = sout.match(new RegExp("Registrant Email: ?([^" + eolToString + "]*)"))
           ;
 
         org = org ? (org[1] ? (org[1].length > 0 ? org[1] : '') : '') : '';
         phone = phone ? (phone[1] ? (phone[1].length > 0 ? phone[1] : '') : '') : '';
         email = email ? (email[1] ? (email[1].length > 0 ? email[1] : '') : '') : '';
-
-        results.push({ domain: domain, available: "no", regOrg: org, regPhone: phone, regEmail: email });
+        saveNewDomain({ domain: domain, available: "no", regOrg: org, regPhone: phone, regEmail: email });
         return cb();
       });
     }
@@ -72,10 +82,6 @@ async.eachOfSeries(data, function (domain, k, cb) {
 }, function afterRun (err) {
   console.log("=========================");
   console.log("Done with domain scraping");
-  var csvified = Papa.unparse(results);
-  process.stdout.write("Writing to " + resultsFile + " ... ");
-  fs.writeFileSync('data/' + resultsFile, csvified, 'utf8');
-  console.log("done");
 });
 
 
